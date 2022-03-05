eb59ae4a0b49b9f3bde9db40eb89c6b332b07506
Austin Caprio <austincaps98@gmail.com>
2022-03-04 22:50:40 -0700

    P5 : V4

--- BookStore/Pages/AddBook.cshtml.cs
+++ BookStore/Pages/AddBook.cshtml.cs
@@ -25,17 +25,13 @@ namespace BookStore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(int bookId, string returnUrl)
         {
             Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
 
-            cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             cart.AddItem(b, 1);
-
-            HttpContext.Session.SetJson("cart", cart);
 
             return RedirectToPage(new { ReturnUrl = returnUrl });
         }
