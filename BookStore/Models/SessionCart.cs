a68368e2e5b504132148d120a2c713b342c7a776
Austin Caprio <austincaps98@gmail.com>
2022-03-04 21:58:59 -0700

    P5 : V2

---
+++ BookStore/Models/SessionCart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using BookStore.Infrastructure;
+using Microsoft.AspNetCore.Http;
+
+namespace BookStore.Models
+{
+    public class SessionCart : Cart
+    {
+        [JsonIgnore]
+        public ISession Session { get; set; }
+
+        public override void AddItem(Book book, int qty)
+        {
+            base.AddItem(book, qty);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveItem(Book book)
+        {
+            base.RemoveItem(book);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void ClearBasket()
+        {
+            base.ClearBasket();
+            Session.Remove("Cart");
+        }
+    }
+}
